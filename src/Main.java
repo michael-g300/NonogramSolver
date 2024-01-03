f1bb8a410e40d1e00f8445227f1549295370817e
Michael <michaelgreyskop@gmail.com>
2024-01-03 13:18:48 +0200

    Phase 4
    given arrays of lines, program produces all acceptable solutions with cell types

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,31 @@ import components.CellSequence;
 import components.CellType;
 import solvers.GenericLineSolver;
 import solvers.SingleCellLineSplitter;
+import solvers.SingleColorSolver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        CellSequence[] array1 = {new CellSequence(1, CellType.BLACK), new CellSequence(2, CellType.BLACK), new CellSequence(3, CellType.BLACK)};
-        CellSequence[] array2 = {new CellSequence(4, CellType.WHITE), new CellSequence(5, CellType.WHITE), new CellSequence(1, CellType.WHITE)};
+        List<Integer> row1 = Arrays.asList(1, 2, 3, 4);
+        List<Integer> row2 = Arrays.asList(1, 3, 6);
+        List<Integer> row3 = Arrays.asList(2, 3, 1);
+        List<Integer> row4 = Arrays.asList(7);
 
-        GenericLineSolver<CellSequence> integerLineSolver = new GenericLineSolver<>();
-        List<List<CellSequence>> combinations = integerLineSolver.allCombinations(array1, array2);
-        List<List<CellType>> isolatedCellCombinations = SingleCellLineSplitter.isolateCells(combinations);
+        SingleColorSolver.solve(Arrays.asList(row1, row2, row3, row4), new ArrayList<List<Integer>>(), 13);
 
-        System.out.println("Combinations:");
-        for (List<CellType> combination : isolatedCellCombinations) {
-            System.out.println(combination);
-        }
+//        CellSequence[] array1 = {new CellSequence(1, CellType.BLACK), new CellSequence(2, CellType.BLACK), new CellSequence(3, CellType.BLACK)};
+//        CellSequence[] array2 = {new CellSequence(4, CellType.WHITE), new CellSequence(5, CellType.WHITE), new CellSequence(1, CellType.WHITE)};
+//
+//        GenericLineSolver<CellSequence> integerLineSolver = new GenericLineSolver<>();
+//        List<List<CellSequence>> combinations = integerLineSolver.allCombinations(array1, array2);
+//        List<List<CellType>> isolatedCellCombinations = SingleCellLineSplitter.isolateCells(combinations);
+//
+//        System.out.println("Combinations:");
+//        for (List<CellType> combination : isolatedCellCombinations) {
+//            System.out.println(combination);
+//        }
     }
 }
--- src/solvers/SingleColorSolver.java
+++ src/solvers/SingleColorSolver.java
@@ -1,107 +1,41 @@
 package solvers;
 
 import components.Board;
+import components.CellSequence;
 import components.CellType;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class SingleColorSolver {
 
-    public static boolean solve(final List<List<Integer>> rows, final List<List<Integer>> cols, final Board board) {
-        checkForOverlap(board, rows, cols);
-        fillWithConstraints(board, rows, cols);
-
+    public static boolean solve(final List<List<Integer>> rows, final List<List<Integer>> cols, final int width) {
+        List<List<List<CellType>>> rowsCombinations = findlinesCombos(rows, width);
+        printResult(rowsCombinations);
         return false;
     }
 
-    private static void checkForOverlap(final Board board, final List<List<Integer>> rows, final List<List<Integer>> cols) {
-        final int rowsNum = board.getRows();
-        final int colsNum = board.getCols();
-        for (int i = 0 ; i < rowsNum ; ++i) {
-            final List<Integer> row = rows.get(i);
-            int coloredCellsNum = countColoredCellsInLine(row);
-            if (row.stream().anyMatch(x -> x > (board.getCols() - coloredCellsNum))) {
-                fillInOverlap(board, i, row, true, board.getCols() - coloredCellsNum);
-            }
+    private static List<List<List<CellType>>> findlinesCombos(final List<List<Integer>> rows, final int width) {
+        List<List<List<CellType>>> rowsCombos = new ArrayList<>();
+        GenericLineSolver<CellSequence> lineSolver = new GenericLineSolver<>();
+        for (List<Integer> row : rows) {
+            CellSequence[] blacks = row.stream().map(length -> new CellSequence(length, CellType.BLACK)).toArray(CellSequence[] :: new);
+            CellSequence[] whites = Arrays.stream(new CellSequence[width - row.stream().mapToInt(a -> a).sum()])
+                    .map(cellSequence -> new CellSequence(1, CellType.WHITE))
+                    .toArray(CellSequence[]::new);
+            rowsCombos.add(SingleCellLineSplitter.isolateCells(lineSolver.allCombinations(blacks, whites)));
         }
-        for (int i = 0 ; i < colsNum ; ++i) {
-            final List<Integer> col = cols.get(i);
-            int coloredCellsNum = countColoredCellsInLine(col);
-            if (col.stream().anyMatch(x -> x > (board.getCols() - coloredCellsNum))) {
-                fillInOverlap(board, i, col, false, board.getCols() - coloredCellsNum);
-            }
-        }
+        return rowsCombos;
     }
 
-    private static void fillInOverlap(final Board board, final int lineNum, final List<Integer> line, final boolean isRow, final int dif) {
-        int startingPosition = 0;
-        for (int num : line) {
-            if (num < dif) {
-                for (int i = startingPosition + dif ; i < num ; ++i) {
-                    board.colorCell(isRow ? lineNum : i, isRow ? i : lineNum, CellType.BLACK);
-                }
-            }
-            startingPosition += (num + 1);
-        }
-    }
-
-    private static int countColoredCellsInLine(final List<Integer> line) {
-        int coloredCellsNum = 0;
-        for (int num : line) {
-            coloredCellsNum += (num + 1);
-        }
-        return coloredCellsNum - 1;
-    }
-
-    private static void fillWithConstraints(final Board board, final List<List<Integer>> rows, final List<List<Integer>> cols) {
-        for (int i = 0 ; i < board.getRows() ; ++i) {
-            for (int j = 0 ; j < board.getCols() ; ++j) {
-                if (board.getCell(i, j) == CellType.BLACK) {
-                    checkFromTop(board, cols.get(j), j, i);
-                    checkFromBottom(board, cols.get(j), i);
-                }
+    private static void printResult(final List<List<List<CellType>>> rowsCombinations) {
+        for (int i = 0 ; i < rowsCombinations.size() ; ++i) {
+            System.out.println("Line " + i + " combinations:");
+            for (List<CellType> combo : rowsCombinations.get(i)) {
+                System.out.println(combo);
             }
         }
-    }
-
-    private static void checkFromTop(final Board board, final List<Integer> col, final int colNum, final int startingPoint) {
-        int freeCells = 0;
-        int coloredAdjacentCells = 0;
-        int completedBlocks = 0;
-        for (int i = 0 ; i < startingPoint ; ++i) {
-            if (board.getCell(i, colNum) == CellType.BLACK) {
-                while (board.getCell(i, colNum) == CellType.BLACK && i < startingPoint) {
-                    ++coloredAdjacentCells;
-                    ++i;
-                }
-                if (i < startingPoint && board.getCell(i, colNum) == CellType.WHITE) {
-                    ++completedBlocks;
-                    coloredAdjacentCells = 0;
-                    freeCells = 0;
-                }
-            }
-            else if (board.getCell(i, colNum) == CellType.WHITE) {
-                if (freeCells != 0) {
-                    return;
-                }
-            }
-            else {
-                ++freeCells;
-            }
-        }
-        final int currentBlock = col.get(completedBlocks);
-        if (board.getCell(startingPoint, colNum) == CellType.BLACK) {
-            final int remainder = currentBlock - coloredAdjacentCells - freeCells;
-            if (remainder == 0 && startingPoint < (board.getRows() - 1)) {
-                board.colorCell(startingPoint + 1, colNum, CellType.WHITE);
-            }
-            for (int i = 0 ; remainder > 0 && i < remainder ; ++i) {
-                board.colorCell(startingPoint + i + 1, colNum, CellType.BLACK);
-            }
-        }
-    }
-
-    private static void checkFromBottom(final Board board, final List<Integer> col, final int startingPoint) {
-
     }
 }
